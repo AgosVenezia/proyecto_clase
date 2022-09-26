9242d03ba8052a94af1d91dd604d01d1f3795caf
Agostina Venezia PC <agostina.cv@gmail.com>
2022-09-26 17:27:27 -0300

    Final clase 10: se agrega archivo urls.py en aplicación cac y se modifica views.py

---
+++ cac/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hola_mundo',views.hola_mundo),
+    path('saludar',views.saludar,name="saludar_por_defecto"),
+    path('saludar/<str:nombre>',views.saludar,name="saludar"),
+    path('proyectos/<int:anio>/<int:mes>',views.ver_proyectos,name="ver_proyectos"),
+]
