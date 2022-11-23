195ce045fd78e6c9011b39dbe6c1e2b8bd89c961
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2022-11-23 16:04:21 -0300

    Final clase 27: personalización del Admin de Django

--- proyecto_clase/urls.py
+++ proyecto_clase/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from cac.admin import mi_admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("cac.urls")),
+    path('cacadmin/', mi_admin.urls),
+    path('',include('cac.urls'))
 ]
